5dcb66282b2272641a38ac5f67718dc813432129
Cleavon Castelino <cleavoncastelino@gmail.com>
2025-06-27 18:10:40 +0100

    Added Basic spinner weapon functionality

---
+++ Basic_Spinner_motor/Basic_Spinner_motor.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+Servo esc;
+
+void setup() {
+  Serial.begin(9600);
+  esc.attach(9);                // Yellow signal wire to D9
+  esc.writeMicroseconds(1000); // Arm
+  Serial.println("Arming ESC...");
+  delay(3000);                  // Give ESC time to arm
+}
+
+void loop() {
+  int throttle = 1300;         // Low throttle
+  esc.writeMicroseconds(throttle);
+  Serial.print("Throttle: ");
+  Serial.println(throttle);
+  delay(1000);
+}
